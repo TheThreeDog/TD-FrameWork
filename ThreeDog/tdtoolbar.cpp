18d076ea4353c6135020a9edd9c7bef2d068b265
ThreeDog <602985142@qq.com>
2017-03-02 10:01:17 +0800

    TD-Framework
    The first version of TD-Framework

---
+++ ThreeDog/tdtoolbar.cpp
@@ -0,0 +1,34 @@
+/***************************************************
+* Program Assigment : 自定义工具栏按钮类
+* File Name: tdtoolbar.cpp
+* Author : ThreeDog
+* Date : 2016/12/28
+* Description: 自定义工具栏类，继承自自定义复选框，几乎没有改动，只是
+*           去除了复选框的右控件，可以通过信号和槽或者回调函数实现功能。
+*
+* **************************************************/
+#include "tdtoolbar.h"
+#include <QMainWindow>
+TDToolbar::TDToolbar(QWidget *parent)
+    :TDCheckbox(parent,NULL)
+{
+    this->setText("");
+}
+
+//最后一个参数毫无意义，只是笨蛋编译器无法区分QString 和 QColor，只能以此来实现构造函数重载
+TDToolbar::TDToolbar(const QString pic_che,const QString pic_non, QWidget *parent)
+    :TDCheckbox(pic_che,pic_non,parent,NULL)
+{
+    this->setText("");
+}
+
+void TDToolbar::setColor(const QColor &col_che, const QColor &col_non)
+{
+    TDCheckbox::setColor(col_che,col_non);
+}
+
+TDToolbar::~TDToolbar()
+{
+
+}
+
